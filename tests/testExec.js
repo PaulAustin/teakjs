b47737da22bd71c2b05dca37c66f1878e7b6baa5
PaulAustin <canoetime@gmail.com>
2016-09-27 08:42:15 -0500

    stdout is now vararg
    using native javascript ‘to sting’ for formatting. This is not ideal.
    need to use teak serialization. For example (1 2 3) should print out (1
    2 3) not 1,2,3

--- tests/testExec.js
+++ tests/testExec.js
@@ -7,13 +7,26 @@ var exec = require('../lib/execution.js');
 var code1 = `
 (
   (startup
-    (stdout 'Hello, World.\n')
+    (stdout 'Hello, World.')
   )
 
   (startup
-    (stdout 'Hello, Moon.\n')
+    (stdout 'Hello, Moon.')
+    (stdout true)
+    (stdout 42)
+    (stdout 1 2 3)
+    (stdout (1 2 3))
   )
 )
+`;
+
+var code1Expected =
+`Hello, World.
+Hello, Moon.
+true
+42
+1 2 3
+1,2,3
 `;
 
 var gOutput = '';
@@ -24,20 +37,28 @@ function pumpLoop(rt) {
     setTimeout(function(){ pumpLoop(rt); }, 100);
   } else {
     console.log('Nothing left to run');
-    console.log(gOutput);
-    assert.equal(gOutput, 'Hello, World.\nHello, Moon.\n');
+    console.log('stdout:', gOutput);
+    assert.equal(gOutput, code1Expected);
   }
 }
-
 
 function test1() {
   let state = {};
   state.stdout = function(text) { gOutput = gOutput + text; };
-
   let obj =  teak.parse(code1, state, exec.standardLib);
-
   let rt = new exec.RunTime(obj, state, exec.standardLib);
   pumpLoop(rt);
 }
+
+var code2 = `
+(
+  (startup
+    (stdout 'Hello, World.\n' 1 2 3)
+  )
+
+  (startup
+  )
+)
+`;
 
 test1();
