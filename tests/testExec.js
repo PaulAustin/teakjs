737d96c005456967b7abece5eac413ea62d5f5a5
PaulAustin <canoetime@gmail.com>
2016-09-28 09:31:29 -0500

    Added more execution test and a bit of a framework for more.

--- tests/testExec.js
+++ tests/testExec.js
@@ -4,7 +4,42 @@ var assert = require('assert');
 var teak = require('../lib/teak.js');
 var exec = require('../lib/execution.js');
 
-var code1 = `
+var gOutput = '';
+var gIndex = 0;
+var gTests = null;
+
+function pumpLoop(rt) {
+  var stuffDone = rt.pump();
+  if (stuffDone) {
+    setTimeout(function(){ pumpLoop(rt); }, 10);
+  } else {
+    console.log('------- Test finished -------');
+    console.log(gOutput);
+    assert.equal(gOutput, gTests[gIndex][1]);
+    gIndex++;
+    if (gIndex < gTests.length) {
+      popAndRunTest();
+    }
+  }
+}
+
+function popAndRunTest(codeBlock) {
+  let state = {};
+  gOutput = '';
+  state.stdout = function(text) { gOutput = gOutput + text; };
+  let obj =  teak.parse(gTests[gIndex][0], state, exec.standardLib);
+  let rt = new exec.RunTime(obj, state, exec.standardLib);
+  pumpLoop(rt);
+}
+
+function runTests(listOfTests) {
+  gTests = listOfTests;
+  gIndex = 0;
+  popAndRunTest();
+}
+
+//------------------------------------------------------------------------------
+let c1 = `
 (
   (startup
     (stdout 'Hello, World.')
@@ -20,7 +55,7 @@ var code1 = `
 )
 `;
 
-var code1Expected =
+let r1 =
 `Hello, World.
 Hello, Moon.
 true
@@ -29,38 +64,61 @@ true
 1,2,3
 `;
 
-var gOutput = '';
-
-function pumpLoop(rt) {
-  var stuffDone = rt.pump();
-  if (stuffDone) {
-    setTimeout(function(){ pumpLoop(rt); }, 10);
-  } else {
-    console.log('Nothing left to run');
-    console.log('stdout:');
-    console.log(gOutput);
-    assert.equal(gOutput, code1Expected);
-  }
-}
-
-function test1(codeBlock) {
-  let state = {};
-  state.stdout = function(text) { gOutput = gOutput + text; };
-  let obj =  teak.parse(codeBlock, state, exec.standardLib);
-  let rt = new exec.RunTime(obj, state, exec.standardLib);
-  pumpLoop(rt);
-}
-
-var code2 = `
+//------------------------------------------------------------------------------
+let c2 = `
 (
   (startup
     (stdout 'Hello, loops.')
-    (loop 2
-      (stdout 'Whats')
-      (stdout 'Up')
+    (loop 3
+      (stdout 'row')
+    )
+    (stdout 'your boat.')
+  )
+)
+`;
+
+let r2 =
+`Hello, loops.
+row
+row
+row
+your boat.
+`;
+
+//------------------------------------------------------------------------------
+let c3 = `
+(
+  (startup
+    (stdout 'Hello, nested loops.')
+    (loop 3
+      (stdout 'a')
+      (loop 2
+        (stdout '  b')
+      )
     )
   )
 )
 `;
 
-test1(code1);
+let r3 =
+`Hello, nested loops.
+a
+  b
+  b
+a
+  b
+  b
+a
+  b
+  b
+`;
+
+
+//------------------------------------------------------------------------------
+let execTests =  [
+  [c1,r1],
+  [c2,r2],
+  [c3,r3],
+];
+
+runTests(execTests);
